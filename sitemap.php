0f23dfd54c10b351f2d0a9d7eb9f5532883343c6
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-04 10:19:02 -0400

    Add files via upload

---
+++ sitemap.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS 1080 My first website!!</title>
+    <meta name="author" content="Tom Redmond">
+    <meta name="description" content="Here is my introduction to building websites on the World Wide Web">
+    <style>
+        figure {
+            float: left;
+            text-align: center;
+            width: 25%;
+        }
+
+        img{
+            max-width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <figure>
+        <img alt="Tom Redmond 2020" src="images/Tom-Redmond.jpg">
+        <figcaption>Photo taken for my UVM id.</figcaption>
+    </figure>
+
+    <h1>CS 1080-<em>B</em> Fall 2023</h1>
+    <h2>Tom Redmond - Site map</h2>
