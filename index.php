f2a9f8d70006fc461f5d9a39553e3f8b6f91a02c
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-07 15:22:34 -0400

    Lab 2: Created Home page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Quality title goes here</title>
+    <meta name="Author" content="Tom Redmond">
+    <meta name="Description" content="Quality description goes here">
+    <!--Make sure to actually put quality title and descriptions in before handing lab 2 into brightspace--> 
+</head>
+
+<body>
+    <main>
+
+    </main>
+
+</body>
