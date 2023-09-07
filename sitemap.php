469639a110cdc14cc09139c2d12e13cfb2cd184d
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-07 14:58:18 -0400

    Lab 2: updated site map

---
+++ sitemap.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS 1080 My first website!!</title>
+    <meta name="author" content="Tom Redmond">
+    <meta name="description" content="Here is my introduction to building websites on the World Wide Web">
+    <style>
+        figure {
+            float: left;
+            text-align: center;
+            width: 25%;
+        }
+
+        img{
+            max-width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <figure>
+        <img alt="Tom Redmond 2020" src="images/Tom-Redmond.jpg">
+        <figcaption>Photo taken for my UVM id.</figcaption>
+    </figure>
+
+    <h1>CS 1080-<em>B</em> Fall 2023</h1>
+    <h2>Tom Redmond - Site map</h2>
+    <p><a href="sample.php">sample.php<a/></p>
+    <p><a href="ADMIN/code-viewer.php">Admin Page</a></p>
+    <section class="lab-layout">
+        <h2 class="header">Lab Two - Choosing your topic.</h2>
+        <section class="public-files">
+            <h3>Public Files</h3>
+            <p><a href="lab2/index.php">Home page</a></p>
+        </section>
+
+        <section class="supporting-files">
+            <h3>Supporting files</h3>
+            <p></p>
+        </section>
+
+        <section class="grader-notes">
+            <h3>Notes to grader</h3>
+            <p></p>
+        </section>
+    </section>
+    
+</body>
+
+</html>
