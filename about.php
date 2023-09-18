92c9d7ea8c3f342fb7784dbf33f1c32ce79bdb28
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-18 10:06:33 -0400

    Lab 3: added new about page

---
+++ about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Human Powered Business</title>
+        <meta name="Author" content="Tom Redmond">
+        <meta name="Description" content="This website will go over how the human powered business model works and why it should be adopted mainstream across corporate America.">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        
+    </head>
+
+    <body>
+        <header>
+            <h1>Human Powered Business Model</h1>
+            <h2>Why businesses should put stakeholders before shareholders</h2>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="detail.php">Example Businesses</a>
+            <a href="form.php">Survey</a>
+        </nav>
+        <main>
+            <h1>More information on the Hunman Powered Business (HPB) model</h1>
+            <section>
+                <h2>Stakeholders before shareholders</h2>
+                <p>Prioritizing stakeholders before shareholders is often associated with a more balanced and sustainable approach to business management. While shareholders are crucial for a company's financial health, focusing solely on their interests can lead to short-term thinking and potentially negative consequences in the long run. Prioritizing stakeholders, on the other hand, can yield several benefits for businesses</p>
+                <cite>My brain</cite>
+            
+            </section>
+            <section>
+                <h2>Benefits of the HPB model</h2>
+                <ul>
+                    <li>Enhanced reputation/brand image</li>
+                    <li>Improved Employee retention/engagement</li>
+                    <li>Increased customer satisfaction/retention</li>
+                    <li><cite>My brain</cite></li>
+                </ul>
+            </section>    
+            <section>
+                <h2>Long term vs short term goals</h2>
+                <p>prioritizing stakeholders before shareholders can lead to a more well-rounded and sustainable approach to business that benefits not only the stakeholders themselves but also the company's long-term success and financial performance. This approach recognizes that a healthy balance between all stakeholder interests can ultimately lead to greater shareholder value.</p>
+                <cite>My brain</cite>
+            </section>
+            <figure>
+                <img src="images/HPB-model.jpg">
+                <figcaption>Shareholders vs Stakeholders</figcaption>
+                <cite><a href="https://www.consultantsreview.com/news/shareholders-vs-stakeholders-a-comparison-nwid-1164.html"></a></cite>
+            </figure>
+        </main>
+        <footer>
+            <p><a href="../index.php">Site map (Main index)</a></p>
+        </footer>
+    </body>
+</html>
