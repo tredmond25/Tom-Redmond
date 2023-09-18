fff5b195d3d49550c7b4b97d06e930c96028da4a
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-18 09:06:57 -0400

    Lab 3: updated site map

--- sitemap.php
+++ sitemap.php
@@ -27,8 +27,30 @@
 
     <h1>CS 1080-<em>B</em> Fall 2023</h1>
     <h2>Tom Redmond - Site map</h2>
-    <p><a href="sample.php">sample.php<a/></p>
+    <p><a href="sample.php">sample.php</a></p>
     <p><a href="ADMIN/code-viewer.php">Admin Page</a></p>
+    <section class="lab-layout">
+        <h2 class="header">Lab Three - Creating a Website.</h2>
+        <section class="public-files">
+            <h3>Public Files</h3>
+            <p><a href="lab3/about.php">About page</a></p>
+            <p><a href="lab3/detail.php">Detail page</a></p>
+            <p><a href="lab3/form.php">Form page</a></p>
+            <p><a href="lab3/index.pho">Home page</a></p>
+        </section>
+
+        <section class="supporting-files">
+            <h3>Supporting files</h3>
+            <p><a href="lab3/css/custom.css">css style sheet</a></p>
+            <p><a href="lab3/wireframe.png">Wireframe</a></p>
+        </section>
+
+        <section class="grader-notes">
+            <h3>Notes to grader</h3>
+            <p></p>
+        </section>
+    </section>
+
     <section class="lab-layout">
         <h2 class="header">Lab Two - Choosing your topic.</h2>
         <section class="public-files">
