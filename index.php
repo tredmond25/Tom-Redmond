25ceb833eb2cca97f199739468bb7103a46d8fea
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-18 09:21:13 -0400

    Lab 3: added header, nav, footer

--- index.php
+++ index.php
@@ -1,65 +1,78 @@
 <!DOCTYPE HTML>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <title>Human Powered Business</title>
-    <meta name="Author" content="Tom Redmond">
-    <meta name="Description" content="This website will go over how the human powered business model works and why it should be adopted mainstream across corporate America.">
-    
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>Human Powered Business</title>
+        <meta name="Author" content="Tom Redmond">
+        <meta name="Description" content="This website will go over how the human powered business model works and why it should be adopted mainstream across corporate America.">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        
+    </head>
 
-<body>
-    <main>
-        <h1>Why Coporate America Should Adopt the Human Powered Business model</h1>
-        <section>
-            <h2>What is the Human Powered Business model?</h2>
-            <p>The term "human-powered business model" typically refers to a business approach that places a strong emphasis on human labor, creativity, and expertise as central components of its operations and value proposition. In such a model, the human element is a key driver of the business's success and differentiation from competitors. Since people are at the forefront of a human-powered business model, their wellbeing should be at the center of the business decisions/ mission statement. Where wellbeing is defined as economic/ financial, social, environmental, and physical health. At the end of the day business is a people endeavor, people either move a business forward or backward so they should be well taken care of by the business.</p>
-            <br>
-            <p>Investing in practices that prioritize the wellbeing of people and the planet might appear expensive initially. However, when we consider the substantial costs associated with pressing issues like climate change, economic inequality, and systemic racial injustice, it becomes evident that these challenges are far more economically burdensome for both individual companies and society as a whole. In the long term, businesses that make substantial investments in promoting the welfare of both people and the environment tend to experience more robust growth, enhanced resilience, and improved financial performance overall.</p>
-        </section>
-        <section>
-            <h2>Why human powered business model should be adopted</h2>
-            <ul>
-                <li>Helps fight economic/wage inequality</li>
-                <li>Helps fight climate change</li>
-                <li>Helps fight racial inequality</li>
-                <li><cite>Smart my brain is (Chuckles like yoda)</cite></li>
-            </ul>
-        </section>    
-        <section>
-            <h2>Key principles of a human powered business</h2>
-            <table>
-                <caption><strong>Key principles</strong></caption>
-                <tr>
-                    <th>Personal Value</th>
-                    <th>Family</th>
-                    <th>Finance</th>
-                </tr>
-                <tr>
-                    <td>Deep respect for others</td>
-                    <td>Real work-life balance</td>
-                    <td>Payed well</td>
-                </tr>
-                <tr>
-                    <td>Personal Growth</td>
-                    <td>Spending time with family/friends</td>
-                    <td>Good benefits</td>
-                </tr>
-                <tr>
-                    <td>Career Growth</td>
-                    <td>Vacation time</td>
-                    <td>Appropriate raises/bonuses</td>
-                </tr>
-                <tr>
-                    <td colspan="3">Citations: <cite>My Brain</cite> 
-                </tr>
-            </table>
-        </section>
-        <figure>
-            <img src="images/Human-Powered-Business.webp">
-            <figcaption>Stock photo showing the connection of people</figcaption>
-        </figure>
-    </main>
-
-</body>
+    <body>
+        <header>
+            <h1>Human Powered Business Model</h1>
+            <h2>Why businesses should put stakeholders before shareholders</h2>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="detail.php">Example Businesses</a>
+            <a href="form.php">Survey</a>
+        </nav>
+        <main>
+            <h1>Why Coporate America Should Adopt the Human Powered Business model</h1>
+            <section>
+                <h2>What is the Human Powered Business model?</h2>
+                <p>The term "human-powered business model" typically refers to a business approach that places a strong emphasis on human labor, creativity, and expertise as central components of its operations and value proposition. In such a model, the human element is a key driver of the business's success and differentiation from competitors. Since people are at the forefront of a human-powered business model, their wellbeing should be at the center of the business decisions/ mission statement. Where wellbeing is defined as economic/ financial, social, environmental, and physical health. At the end of the day business is a people endeavor, people either move a business forward or backward so they should be well taken care of by the business.</p>
+                <br>
+                <p>Investing in practices that prioritize the wellbeing of people and the planet might appear expensive initially. However, when we consider the substantial costs associated with pressing issues like climate change, economic inequality, and systemic racial injustice, it becomes evident that these challenges are far more economically burdensome for both individual companies and society as a whole. In the long term, businesses that make substantial investments in promoting the welfare of both people and the environment tend to experience more robust growth, enhanced resilience, and improved financial performance overall.</p>
+            </section>
+            <section>
+                <h2>Why human powered business model should be adopted</h2>
+                <ul>
+                    <li>Helps fight economic/wage inequality</li>
+                    <li>Helps fight climate change</li>
+                    <li>Helps fight racial inequality</li>
+                    <li><cite>Smart my brain is (Chuckles like yoda)</cite></li>
+                </ul>
+            </section>    
+            <section>
+                <h2>Key principles of a human powered business</h2>
+                <table>
+                    <caption><strong>Key principles</strong></caption>
+                    <tr>
+                        <th>Personal Value</th>
+                        <th>Family</th>
+                        <th>Finance</th>
+                    </tr>
+                    <tr>
+                        <td>Deep respect for others</td>
+                        <td>Real work-life balance</td>
+                        <td>Payed well</td>
+                    </tr>
+                    <tr>
+                        <td>Personal Growth</td>
+                        <td>Spending time with family/friends</td>
+                        <td>Good benefits</td>
+                    </tr>
+                    <tr>
+                        <td>Career Growth</td>
+                        <td>Vacation time</td>
+                        <td>Appropriate raises/bonuses</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">Citations: <cite>My Brain</cite> 
+                    </tr>
+                </table>
+            </section>
+            <figure>
+                <img src="images/Human-Powered-Business.webp">
+                <figcaption>Stock photo showing the connection of people</figcaption>
+            </figure>
+        </main>
+        <footer>
+            <p><a href="../index.php">Site map (Main index)</a></p>
+        </footer>
+    </body>
+</html>
