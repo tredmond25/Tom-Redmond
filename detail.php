fac59808afe3be91162ed058159a325757445b53
tredmond25 <143559895+tredmond25@users.noreply.github.com>
2023-09-18 09:44:37 -0400

    Lab 3: added new detail page

---
+++ detail.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Human Powered Business</title>
+        <meta name="Author" content="Tom Redmond">
+        <meta name="Description" content="This website will go over how the human powered business model works and why it should be adopted mainstream across corporate America.">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        
+    </head>
+
+    <body>
+        <header>
+            <h1>Human Powered Business Model</h1>
+            <h2>Why businesses should put stakeholders before shareholders</h2>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="detail.php">Example Businesses</a>
+            <a href="form.php">Survey</a>
+        </nav>
+        <main>
+            <h1>Businesses that lead the way in the HPB Model</h1>
+            <section>
+                <h2>New Belgium Brewing</h2>
+                <p>New Belgium believes in a Human Powered Business approach is needed in corporate America to handle/ start to correct climate change, widespread economic inequality, and systemic racism.</p>
+                <br>
+                <p>By doing right by the people/ having fun New Belgium has created an excellent working environment that enables better results for their sustainability goals. As a 100% employee-owned business, they offer great pay and benefits, but also, they have a Human Powered Governance system which empowers coworkers to participate in key decisions and elevate feedback or input directly to leadership. Which in turn creates a high involvement culture where their coworkers have a greater sense of acknowledgment and responsibility for their decisions. New Belgium, through many climate initiatives and by putting people first they have been able to reach success in their sustainability initiatives. Even where they have fallen short with their packaging, they have still done a great job of being transparent and advocating for the consumer to recycle by co-founding the Glass Recycling Coalition.</p>
+                <cite><a href="https://www.newbelgium.com/">New Belgium information</a></cite>
+            </section>
+            <section>
+                <h2>New Belgium accomplishments</h2>
+                <ul>
+                    <li>Financial transparency</li>
+                    <li>Leading Minimum Wages</li>
+                    <li>Robust Benefits Package</li>
+                    <li>High-Involvement Culture</li>
+                    <li><cite><a href="https://www.newbelgium.com/company/mission/do-right-by-people/">New Belgium information</a></cite></li>
+                </ul>
+            </section>    
+            <section>
+                <h2>New Belgium's Climate initiatives</h2>
+                <ul>
+                    <li>Works Hard to reduce emissions</li>
+                    <li>LEED Certifications on breweries</li>
+                    <li>Started the Glass Recycling Coalition to improve recycling nationwide</li>
+                    <li><cite><a href="https://www.newbelgium.com/company/mission/climate/">New Belgium information</a></cite></li>
+            </section>
+            <figure>
+                <img src="images/newbelgiumlogo.jpg">
+                <figcaption>New Belgium's logo</figcaption>
+                <cite><a href="https://www.newbelgium.com/">New belgium logo</a></cite>
+            </figure>
+        </main>
+        <footer>
+            <p><a href="../index.php">Site map (Main index)</a></p>
+        </footer>
+    </body>
+</html>
